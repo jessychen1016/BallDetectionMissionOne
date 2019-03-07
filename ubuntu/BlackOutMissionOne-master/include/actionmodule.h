04f8a1a77952d5adc974d88c8e29231f6d7edacb
jessychen1016 <chenzexi1016@126.com>
2019-03-07 13:50:53 +0800

    the first one on brix

---
+++ ubuntu/BlackOutMissionOne-master/include/actionmodule.h
@@ -0,0 +1,32 @@
+#ifndef __ACTIONMODULE_H__
+#define __ACTIONMODULE_H__
+
+#include <QUdpSocket>
+#include "singleton.hpp"
+#include <QObject>
+
+class ActionModule: public QObject{
+	Q_OBJECT
+public:
+	ActionModule(QObject * parent = 0);
+	~ActionModule();
+	void sendPacket(quint8 id, qint16 vx=0, qint16 vy=0, qint16 vr=0, bool is_dribble = false);
+	bool getInfrared(){
+		return infrared;
+	}
+	void readData();
+private:
+	void sendStartPacket();
+	quint8 id = 3;
+	QByteArray tx;
+	QByteArray rx;
+	QUdpSocket sendSocket;
+	QUdpSocket receiveSocket;
+	bool infrared = false;
+
+};
+
+typedef Singleton<ActionModule> ZActionModule;
+
+
+#endif // __ACTIONMODULE_H__
